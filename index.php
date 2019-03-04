20f4d395a2856dc488b3b177fbe112e230728a7f
Alexander Kolin <alexander.kolin@myomegasys.com>
2019-03-04 16:50:23 +0100

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+* @author Alexander Kolin
+-->
+<html>
+    <head>
+        <meta charset="windows-1252">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo "Hello World!";
+        ?>
+    </body>
+</html>
